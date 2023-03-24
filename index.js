822386e5936a49fdeccdf18281e41c9051dd9acb
Meet Mistry <meetmistry1510@gmail.com>
2023-03-24 22:39:34 +0530

    server done

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const Book = require('./book');
-const db = require('./db');
+const express = require("express");
+const Book = require("./book");
+const db = require("./db");
 
 const app = express();
 
-app.get('/books', async (req, res, next) => {
+app.get("/books", async (req, res, next) => {
   try {
     const books = await Book.find();
     res.json(books);
@@ -15,12 +15,12 @@ app.get('/books', async (req, res, next) => {
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+  res.status(500).send("Something went wrong!");
 });
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  app.listen(3000, () => {
-    console.log('Server started on port 3000');
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server started on port ${process.env.PORT || "3000"}`);
   });
 });
