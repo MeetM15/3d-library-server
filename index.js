4c51a0c5a212d4ea45b301798bbf03f941b55d02
Meet Mistry <meetmistry1510@gmail.com>
2023-04-01 14:11:30 +0530

    Added two more endpoints

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const Book = require("./book");
 const db = require("./db");
-
+const cors = require("cors");
 const app = express();
-
+app.use(cors());
 app.get("/books", async (req, res, next) => {
   try {
     const books = await Book.find();
@@ -12,6 +12,29 @@ app.get("/books", async (req, res, next) => {
     next(err);
   }
 });
+app.get("/categories", async (req, res, next) => {
+  try {
+    const categories = await Book.find({}, "Category");
+    const uniqueCategories = [
+      ...new Set(categories.map((item) => item._doc.Category)),
+    ];
+    res.json(uniqueCategories);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/books/:category', async (req, res) => {
+  const { category } = req.params;
+
+  try {
+    const books = await Book.find({ Category: category });
+    res.status(200).json(books);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
